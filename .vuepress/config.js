5ccbfdfa44c2d343274b57f91fb9a0e9923597c0
FeedWhisper5148 <99077155+FeedWhisper5148@users.noreply.github.com>
2022-08-15 17:01:59 +0800

    Update config.js

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -52,8 +52,8 @@ module.exports = {
     sidebar: {
       '/docs/theme-reco/': [
         '',
-        'theme',
-        'plugin',
+        'genshin',
+        'yunzaibot',
         'api'
       ]
     },  
